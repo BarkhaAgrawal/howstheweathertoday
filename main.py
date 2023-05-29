dbd1ea723e7ec6d75aac17d08fe63f1d60766b83
Barkha Agrawal <barkha@Barkhas-MacBook-Pro.local>
2023-05-29 13:33:52 -0700

    bug fix: missing '

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 def get_weather_data(city):
-    api_key = 'yourAPIKey
+    api_key = 'yourAPIKey'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
     if response.status_code == 200:
